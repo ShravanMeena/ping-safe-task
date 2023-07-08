686bccb15c5a941a33d1b2206c961b05c6ebf59e
shravanmeena <shravanmeena47@gmail.com>
2023-07-08 17:38:01 +0530

    added'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { directoryData } from "./data";
 
-function App() {
+const CheckBoxTree = ({ data, selected, setSelected, depth = 0 }) => {
+  const isIndeterminate = (node) => {
+    if (!node.children) {
+      return false;
+    }
+    const childState = node.children.map((child) => selected[child.id]);
+    return childState.includes(true) && childState.includes(false);
+  };
+
+  const isChecked = (node) => {
+    if (!node.children) {
+      return !!selected[node.id];
+    }
+    return node.children.every(isChecked);
+  };
+
+  const toggleNode = (node, status) => {
+    setSelected((prevSelected) => {
+      const updated = { ...prevSelected, [node.id]: status };
+      if (node.children) {
+        node.children.forEach((n) => {
+          updated[n.id] = status;
+          if (n.children) {
+            toggleNode(n, status);
+          }
+        });
+      }
+      return updated;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul>
+      {data.map((node) => {
+        const checked = isChecked(node);
+        return (
+          <li key={node.id} style={{ marginLeft: depth * 20 }}>
+            <label>
+              <input
+                type="checkbox"
+                checked={checked}
+                ref={input => input && (input.indeterminate = isIndeterminate(node))}
+                onChange={(e) => {
+                  toggleNode(node, e.target.checked);
+                }}
+              />
+              {node.type === "folder" ? <b>{node.name}</b> : node.name}
+            </label>
+            {node.children && (
+              <CheckBoxTree
+                data={node.children}
+                selected={selected}
+                setSelected={setSelected}
+                depth={depth + 1}
+              />
+            )}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default function App() {
+  const [selected, setSelected] = useState({});
+  const [submitData, setSubmitData] = useState([]);
+  
+  const handleSubmit = () => {
+    const paths = [];
+    const traverse = (node, path) => {
+      if (selected[node.id]) {
+        paths.push(path);
+      }
+      node.children?.forEach((child) => traverse(child, `${path}/${child.name}`));
+    };
+    directoryData.forEach((node) => traverse(node, node.name));
+    setSubmitData(paths);
+  };
+
+  useEffect(() => {
+    const initSelected = {};
+    const initNodes = (nodes) => {
+      nodes.forEach((node) => {
+        initSelected[node.id] = false;
+        if (node.children) {
+          initNodes(node.children);
+        }
+      });
+    };
+    initNodes(directoryData);
+    setSelected(initSelected);
+  }, []);
+
+  return (
+    <div>
+      <CheckBoxTree data={directoryData} selected={selected} setSelected={setSelected} />
+      <button onClick={handleSubmit}>Submit</button>
+      <ul>
+        {submitData.map((path, index) => (
+          <li key={index}>{path}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const directoryData = [
+  {
+    id: 1,
+    name: "Folder 1",
+    type: "folder",
+    children: [
+      {
+        id: 2,
+        name: "File 1",
+        type: "file",
+      },
+      {
+        id: 3,
+        name: "Folder 2",
+        type: "folder",
+        children: [
+          {
+            id: 4,
+            name: "File 2",
+            type: "file",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "File 3",
+    type: "file",
+  },
+];
